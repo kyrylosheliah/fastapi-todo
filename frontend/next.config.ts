570fb3a4866ae74fe907156241b20523c0e7ff27
kyrylosheliah <kyrylosheliah@gmail.com>
2025-10-03 23:17:38 +0300

    stop using static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
   },
-  output: "export",
 };
 
 export default nextConfig;
